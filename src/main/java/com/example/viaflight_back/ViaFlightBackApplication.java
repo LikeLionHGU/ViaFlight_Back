195d59ec4eb263d2862235597ed8fd125315711a
7SH7 <kimseunghwan7777@gmail.com>
2024-02-14 01:07:13 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/viaflight_back/ViaFlightBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.viaflight_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaFlightBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViaFlightBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/viaflight_back/ViaFlightBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viaflight_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaFlightBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
